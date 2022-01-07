fe670bd5ebb2eb2e8070bbd1999056928cbd46ce
Prajwalhn18 <prajwalhaniya18@gmail.com>
2022-01-07 21:53:33 +0530

    creative-coding

--- Maze-generator/script.js
+++ Maze-generator/script.js
@@ -1,9 +1,11 @@
 // const canvasWidth = document.getElementById("enteredWidth");
 // const canvasHeight = document.getElementById("enteredWidth");
 var columns, rows;
-var w = 50;
+var w = 20;
 var grid = [];
 var current;
+
+var stack = [];
 
 function setup() {
     createCanvas(500, 500);
@@ -25,12 +27,17 @@ function draw() {
         grid[i].show();
     }
     current.visited = true;
+    current.highLight();
     var next = current.checkNeighbors();
     if (next) {
         next.visited = true;
+        stack.push(current);
+        removeWalls(current, next);
         current = next;
+    } else if (stack.length > 0) {
+        current = stack.pop();
     }
-    frameRate(5);
+    frameRate(10);
 }
 
 function index(i, j) {
@@ -45,32 +52,38 @@ function Cell(i, j) {
     this.visited = false;
 
     this.checkNeighbors = function () {
-        var neighbrors = [];
+        var neighbors = [];
 
         var top = grid[index(i, j - 1)];
         var right = grid[index(i + 1, j)];
         var bottom = grid[index(i, j + 1)];
         var left = grid[index(i - 1, j)];
 
-        if (top && !top.visited) neighbrors.push(top);
-        if (right && !right.visited) neighbrors.push(right);
-        if (bottom && !bottom.visited) neighbrors.push(bottom);
-        if (left && !left.visited) neighbrors.push(left);
+        if (top && !top.visited) neighbors.push(top);
+        if (right && !right.visited) neighbors.push(right);
+        if (bottom && !bottom.visited) neighbors.push(bottom);
+        if (left && !left.visited) neighbors.push(left);
 
-        if (neighbrors.length > 0) {
-            var randomNumber = Math.floor(
-                Math.random(0, neighbrors.length) * 2
-            );
-            return neighbrors[randomNumber];
+        if (neighbors.length > 0) {
+            var randomNumber = floor(random(0, neighbors.length));
+            return neighbors[randomNumber];
         } else {
             return undefined;
         }
     };
 
+    this.highLight = function () {
+        var x = this.i * w;
+        var y = this.j * w;
+        stroke(220, 220, 220);
+        noStroke();
+        fill(220, 0, 225, 100);
+        rect(x, y, w, w);
+    };
     this.show = function () {
         var x = this.i * w;
         var y = this.j * w;
-        stroke(220, 120, 20);
+        stroke(220, 220, 220);
 
         if (this.walls[0]) line(x, y, x + w, y);
         if (this.walls[1]) line(x + w, y, x + w, y + w);
@@ -78,8 +91,29 @@ function Cell(i, j) {
         if (this.walls[3]) line(x, y + w, x, y);
 
         if (this.visited) {
+            noStroke();
             fill(255, 170, 0);
             rect(x, y, w, w);
         }
     };
+}
+
+function removeWalls(a, b) {
+    var x = a.i - b.i;
+    if (x == 1) {
+        a.walls[3] = false;
+        b.walls[1] = false;
+    } else if (x == -1) {
+        a.walls[1] = false;
+        b.walls[3] = false;
+    }
+
+    var y = a.j - b.j;
+    if (y == 1) {
+        a.walls[0] = false;
+        b.walls[2] = false;
+    } else if (y == -1) {
+        a.walls[2] = false;
+        b.walls[0] = false;
+    }
 }
